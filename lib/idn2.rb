fc0da0878a7541cc4efe4e0d513422a6ed3026ba
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-04-12 08:08:51 +0900

    rubocop

--- lib/idn2.rb
+++ lib/idn2.rb
@@ -1,5 +1,5 @@
-require "idn2/version"
-require "idn2/idn2"
+require 'idn2/version'
+require 'idn2/idn2'
 
 module Idn2
   # Your code goes here...
--- lib/idn2/version.rb
+++ lib/idn2/version.rb
@@ -1,3 +1,3 @@
 module Idn2
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'.freeze
 end
