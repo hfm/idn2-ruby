7cacf99d286b481ba5beb5666271afbc425bb331
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-04-14 03:50:33 +0900

    Add flags and rename header

--- test/idn2_test.rb
+++ test/idn2_test.rb
@@ -8,4 +8,13 @@ class Idn2Test < Minitest::Test
   def test_that_it_has_a_library_version_number
     refute_nil ::Idn2::IDN2_VERSION
   end
+
+  def test_that_it_has_idn2_flags
+    assert 1, Idn2::NFC_INPUT
+    assert 2, Idn2::ALABEL_ROUNDTRIP
+    assert 4, Idn2::TRANSITIONAL
+    assert 8, Idn2::NONTRANSITIONAL
+    assert 16, Idn2::ALLOW_UNASSIGNED
+    assert 32, Idn2::USE_STD3_ASCII_RULES
+  end
 end
