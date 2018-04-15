5aac68411149b28930289116195d336a6b964374
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-04-15 23:49:59 +0900

    change

--- ext/idn2/extconf.rb
+++ ext/idn2/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
 
 dir_config('idn2')
-create_makefile('idn2/idn2') if have_library('idn2') && have_header('idn2.h')
+have_library('idn2')
+have_header('idn2.h')
+create_makefile('idn2/idn2')
