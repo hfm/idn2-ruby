4a2ca409b5395777acfa4e9a46c8ce961eb65956
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-04-17 22:59:47 +0900

    clang-format

--- ext/idn2/idn2.c
+++ ext/idn2/idn2.c
@@ -3,17 +3,16 @@
 
 VALUE rb_mIdn2;
 
-void
-Init_idn2(void)
+void Init_idn2(void)
 {
   rb_mIdn2 = rb_define_module("Idn2");
 
   rb_define_const(rb_mIdn2, "IDN2_VERSION", rb_str_new2(IDN2_VERSION));
 
-  rb_define_const(rb_mIdn2, "NFC_INPUT",            INT2FIX(IDN2_NFC_INPUT));
-  rb_define_const(rb_mIdn2, "ALABEL_ROUNDTRIP",     INT2FIX(IDN2_ALABEL_ROUNDTRIP));
-  rb_define_const(rb_mIdn2, "TRANSITIONAL",         INT2FIX(IDN2_TRANSITIONAL));
-  rb_define_const(rb_mIdn2, "NONTRANSITIONAL",      INT2FIX(IDN2_NONTRANSITIONAL));
-  rb_define_const(rb_mIdn2, "ALLOW_UNASSIGNED",     INT2FIX(IDN2_ALLOW_UNASSIGNED));
+  rb_define_const(rb_mIdn2, "NFC_INPUT", INT2FIX(IDN2_NFC_INPUT));
+  rb_define_const(rb_mIdn2, "ALABEL_ROUNDTRIP", INT2FIX(IDN2_ALABEL_ROUNDTRIP));
+  rb_define_const(rb_mIdn2, "TRANSITIONAL", INT2FIX(IDN2_TRANSITIONAL));
+  rb_define_const(rb_mIdn2, "NONTRANSITIONAL", INT2FIX(IDN2_NONTRANSITIONAL));
+  rb_define_const(rb_mIdn2, "ALLOW_UNASSIGNED", INT2FIX(IDN2_ALLOW_UNASSIGNED));
   rb_define_const(rb_mIdn2, "USE_STD3_ASCII_RULES", INT2FIX(IDN2_USE_STD3_ASCII_RULES));
 }
